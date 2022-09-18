65d603b250470670faddd9311563a9203e0d68c3
Abdalrahman Alaa <103855994+Abdalrahman1723@users.noreply.github.com>
2022-09-18 19:36:31 +0200

    the answer

---
+++ p520A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+    int n;
+    string s;
+    int f = 0;
+    cin >> n;
+    cin >> s;
+    if (n < 26)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        for (int i = 65; i <= 90; i++)
+        {
+            for (int a = 0; a < n; a++)
+            {
+                s[a] = toupper(s[a]);
+                if (s[a] == i)
+                {
+                    f++;
+                    break;
+                }
+            }
+        }
+        if (f == 26)
+        {
+            cout << "YES";
+        }
+        else
+            cout << "NO";
+    }
+    return 0;
+}
